edb2af30eb0b6fb0c21235f6db9cc297fdf3af7e
Theelipan Prabakar <theelipan@gmail.com>
2020-08-06 20:24:00 -0400

    array data

---
+++ app.js
@@ -0,0 +1,17 @@
+const businesses = [
+  { name: "business One", category: "Finance", start: 1981, end: 2004 },
+  { name: "business Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "business Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "business Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "business Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "business Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "business Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "business Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "business Nine", category: "Retail", start: 1981, end: 1989 },
+];
+
+// bussinesses.forEach((element) => {
+//   console.log(businesses);
+// });
+
+console.log(businesses);
