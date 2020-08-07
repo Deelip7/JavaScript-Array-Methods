0e267e4c9e4e6c8d7288858ecda20abcf825b66f
Theelipan Prabakar <theelipan@gmail.com>
2020-08-06 21:17:15 -0400

    Add methods

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const businesses = [
-  { name: "business One", category: "Finance", start: 1981, end: 2004 },
-  { name: "business Two", category: "Retail", start: 1992, end: 2008 },
-  { name: "business Three", category: "Auto", start: 1999, end: 2007 },
-  { name: "business Four", category: "Retail", start: 1989, end: 2010 },
-  { name: "business Five", category: "Technology", start: 2009, end: 2014 },
-  { name: "business Six", category: "Finance", start: 1987, end: 2010 },
-  { name: "business Seven", category: "Auto", start: 1986, end: 1996 },
-  { name: "business Eight", category: "Technology", start: 2011, end: 2016 },
-  { name: "business Nine", category: "Retail", start: 1981, end: 1989 },
+const business = [
+  { name: "Prime", category: "Finance", start: 1981, end: 2004 },
+  { name: "Marspoint", category: "Retail", start: 1992, end: 2008 },
+  { name: "Aces", category: "Auto", start: 1999, end: 2007 },
+  { name: "Hurricane Lighting", category: "Retail", start: 1989, end: 2010 },
+  { name: "Eclipse", category: "Technology", start: 2009, end: 2014 },
+  { name: "Globestar", category: "Finance", start: 1987, end: 2010 },
+  { name: "Vertex", category: "Auto", start: 1986, end: 1996 },
+  { name: "Silverstar", category: "Technology", start: 2011, end: 2016 },
+  { name: "Ravenworks", category: "Retail", start: 1981, end: 1989 },
 ];
 
-// bussinesses.forEach((element) => {
-//   console.log(businesses);
-// });
+//Filter Methods -------------------------
+const fileredTypes = business.filter((e) => {
+  return e.category === "Auto";
+});
+console.log(fileredTypes);
 
-console.log(businesses);
+//Map Method -------------------------
+
+const mappedTypes = business.map((e) => {
+  return e.name;
+});
+console.log(mappedTypes);
+
+// Find Method -------------------------
+const findTypes = business.find((e) => {
+  return e.name === "Eclipse";
+});
+console.log(findTypes);
+
+// forEach Method -------------------------
+business.forEach((e) => {
+  console.log(e.name);
+});
