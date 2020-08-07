b9da87ca893fc05615231efb2267b29631aec946
Theelipan Prabakar <theelipan@gmail.com>
2020-08-06 21:43:11 -0400

    all methods addede

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
 const business = [
-  { name: "Prime", category: "Finance", start: 1981, end: 2004 },
-  { name: "Marspoint", category: "Retail", start: 1992, end: 2008 },
-  { name: "Aces", category: "Auto", start: 1999, end: 2007 },
-  { name: "Hurricane Lighting", category: "Retail", start: 1989, end: 2010 },
-  { name: "Eclipse", category: "Technology", start: 2009, end: 2014 },
-  { name: "Globestar", category: "Finance", start: 1987, end: 2010 },
-  { name: "Vertex", category: "Auto", start: 1986, end: 1996 },
-  { name: "Silverstar", category: "Technology", start: 2011, end: 2016 },
-  { name: "Ravenworks", category: "Retail", start: 1981, end: 1989 },
+  { name: "Prime", category: "Finance", start: 1981, revenue: 24 },
+  { name: "Marspoint", category: "Retail", start: 1992, revenue: 28 },
+  { name: "Aces", category: "Auto", start: 1999, revenue: 17 },
+  { name: "Hurricane Lighting", category: "Retail", start: 1989, revenue: 10 },
+  { name: "Eclipse", category: "Technology", start: 2009, revenue: 11 },
+  { name: "Globestar", category: "Finance", start: 1987, revenue: 16 },
+  { name: "Vertex", category: "Auto", start: 1986, revenue: 16 },
+  { name: "Silverstar", category: "Technology", start: 2011, revenue: 21 },
+  { name: "Ravenworks", category: "Retail", start: 1981, revenue: 19 },
 ];
 
 //Filter Methods -------------------------
 const fileredTypes = business.filter((e) => {
   return e.category === "Auto";
 });
-console.log(fileredTypes);
+console.log("fileredTypes", fileredTypes);
 
 //Map Method -------------------------
-
 const mappedTypes = business.map((e) => {
-  return e.name;
+  return e.start;
 });
-console.log(mappedTypes);
+console.log("mappedTypes", mappedTypes);
 
 // Find Method -------------------------
 const findTypes = business.find((e) => {
   return e.name === "Eclipse";
 });
-console.log(findTypes);
+console.log("findTypes", findTypes);
 
 // forEach Method -------------------------
 business.forEach((e) => {
-  console.log(e.name);
+  console.log("forEach - Name:", e.name);
 });
+
+// Some Method -------------------------
+const someTypes = business.some((e) => {
+  return e.start >= 2019;
+});
+console.log("someTypes", someTypes);
+
+// Every Method -------------------------
+const everyTypes = business.every((e) => {
+  return e.start >= 1981;
+});
+console.log("everyTypes", everyTypes);
+
+// Reduce Method -------------------------
+const revenueTotal = business.reduce((currentTotal, e) => {
+  return e.revenue + currentTotal;
+}, 0);
+
+console.log("revenueTotal", revenueTotal);
+
+// Includes Method -------------------------
+const includesTypes = business.includes(1);
+
+console.log("revenueTotal", includesTypes);
